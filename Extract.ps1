01e806b50c153b3df3bc1d284e5b5679d4a0dc6f
Daniel Keer <4249262+thedxt@users.noreply.github.com>
2026-01-04 10:02:13 -0700

    Update Extract.ps1
    added URI and changed write-verbose to write-host

--- Extract.ps1
+++ Extract.ps1
@@ -3,9 +3,11 @@
 # Function: Extract
 #
 # Contributors: @theDXT
-# Created: 2024-12-19
-# Last Modified: 2024-12-19
-# Version 1.0.0
+# Created: 2024-Dec-19
+# Last Modified: 2026-Jan-04
+# Version 1.0.1
+#
+# Script URI: https://github.com/thedxt/Install-Matrix
 #
 # Description:
 #   extracts the specified zip and exports it to the specified output path.
@@ -25,9 +27,10 @@ function Extract{
     try{
 Write-verbose "Extracting zip"
 Expand-Archive -force $ZipFile -DestinationPath $OutputPath
-Write-verbose "zip is extracted to: $OutputPath"
+Write-host "zip is extracted to: $OutputPath"
 } catch{
-Write-Verbose "Error extracting zip : $_"
+Write-host "Error extracting zip : $_"
         Exit 1
 }
+
 }
