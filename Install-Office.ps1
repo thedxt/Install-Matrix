83a38086209ee210b33e3f744f8413e3320e11d3
Daniel Keer <4249262+thedxt@users.noreply.github.com>
2026-01-04 10:08:12 -0700

    Update Install-Office.ps1
    added URI and changed write-verbose to write-host

--- Install-Office.ps1
+++ Install-Office.ps1
@@ -3,9 +3,12 @@
 # Function: Install-Office
 #
 # Contributors: @kaysouthall, @theDXT
-# Created: 2024-10-07
-# Last Modified: 2025-01-06
-# Version 3.0
+# Created: 2024-Oct-07
+# Last Modified: 2026-Jan-04
+# Version 3.0.1
+#
+# Script URI: https://github.com/thedxt/Install-Matrix
+#
 # Description:
 #   Executes the downloaded installer with specified arguments to perform a sillent installation.
 #
@@ -22,12 +25,13 @@ function Install-Office {
     )
     
     try {
-    Write-Verbose "Installation is starting"
+    Write-Host "Installation is starting"
        Start-Process $InstallerPath $FullInstallArgs -wait -WindowStyle Hidden
 
-        Write-Verbose "Installation completed successfully"
+        Write-Host "Installation completed successfully"
     } catch {
-        Write-Verbose "Error during installation: $_"
+        Write-Host "Error during installation: $_"
         Exit 1
     }
+
 }
